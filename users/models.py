6488282df6a7bcf578c07469f1ba2c1ce5dfe00f
karakure178 <egg9017shun@gmail.com>
2022-04-12 03:34:00 +0900

    [cha_dev] header_add

--- users/models.py
+++ users/models.py
@@ -8,14 +8,21 @@ class User(AbstractUser):
     class Meta:
         db_table = 'auth_user'
     age = models.IntegerField(verbose_name="年齢",default=20)
-    icon = models.ImageField(verbose_name="アイコン", 
-    blank= True, 
-    upload_to = "profile/icon/", 
-    default="profile/icon/default.jpg",
+    icon = models.ImageField(
+        verbose_name="アイコン", 
+        blank= True, 
+        upload_to = "profile/icon/", 
+        default="profile/icon/default.jpg",
     )
     user_id =  models.CharField(
         verbose_name="ユーザーID",
         default= str(ULID()),#str(ULID()),
         max_length=26,#primary_key=True,
         # editable=False
+    )
+    header = models.ImageField(
+        verbose_name="プロフィールヘッダー", 
+        blank= True, 
+        upload_to = "profile/header/", 
+        default="profile/header/default.jpg",
     )
