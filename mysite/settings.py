9456ef308e90f53d9769f5f2084a8cc126d98474
karakure178 <egg9017shun@gmail.com>
2022-04-13 03:30:08 +0900

    all

--- mysite/settings.py
+++ mysite/settings.py
@@ -94,10 +94,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_DIRS = (
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
@@ -140,4 +140,4 @@ except ImportError:
 if not DEBUG:
     #SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
-    django_heroku.settings(locals(),staticfiles=False)
+    django_heroku.settings(locals())
